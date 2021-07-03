c48fa18e4bfa5a8d3f3854d4041e9acb85fdbe2b
Arnaldo C. Bezerra <arnaldo.bezerra@waitrose.co.uk>
2021-07-04 00:16:06 +0100

    Refactoring

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SigninComponent } from './signin/signin.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { VMessageModule } from 'app/shared/vmessage/vmessage.module';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SigninComponent } from "./signin/signin.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { VMessageModule } from "app/shared/vmessage/vmessage.module";
 
 @NgModule({
   declarations: [SigninComponent, HomePageComponent],
