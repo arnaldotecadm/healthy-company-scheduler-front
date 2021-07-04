a2235aa84126642fcdb525c59002bda18d80420e
Arnaldo C. Bezerra <arnaldo.bezerra@waitrose.co.uk>
2021-07-05 00:16:17 +0100

    Refactoring

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,23 +1,23 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-import { UserComponent } from '../../user/user.component';
-import { AppListComponent } from 'app/aplicativo/app-list/app-list.component';
-import { SoftwareComponent } from 'app/aplicativo/software/software.component';
-import { AuthGuard } from 'app/core/auth/auth.guard';
-import { HomePageComponent } from 'app/home/home-page/home-page.component';
-import { UserListComponent } from 'app/user/user-list/user-list.component';
-import { UpdateListComponent } from 'app/aplicativo/update-list/update-list.component';
-import { UpdateComponent } from 'app/aplicativo/update/update.component';
+import { UserComponent } from "../../user/user.component";
+import { AppListComponent } from "app/aplicativo/app-list/app-list.component";
+import { SoftwareComponent } from "app/aplicativo/software/software.component";
+import { AuthGuard } from "app/core/auth/auth.guard";
+import { HomePageComponent } from "app/home/home-page/home-page.component";
+import { UserListComponent } from "app/user/user-list/user-list.component";
+import { UpdateListComponent } from "app/aplicativo/update-list/update-list.component";
+import { UpdateComponent } from "app/aplicativo/update/update.component";
 
 export const AdminLayoutRoutes: Routes = [
-  { path: 'home', component: HomePageComponent, canActivate: [AuthGuard] },
+  { path: "home", component: HomePageComponent, canActivate: [AuthGuard] },
   {
-    path: 'user',
+    path: "user",
     component: UserComponent,
   },
-  { path: 'user-list', component: UserListComponent },
-  { path: 'app-list', component: AppListComponent },
-  { path: 'software', component: SoftwareComponent },
-  { path: 'update-list', component: UpdateListComponent },
-  { path: 'update', component: UpdateComponent },
+  { path: "user-list", component: UserListComponent },
+  { path: "app-list", component: AppListComponent },
+  { path: "software", component: SoftwareComponent },
+  { path: "update-list", component: UpdateListComponent },
+  { path: "update", component: UpdateComponent },
 ];
