d386f808006ae761308947bb901c80c19b22407f
Arnaldo C. Bezerra <arnaldo.bezerra@waitrose.co.uk>
2021-07-06 00:16:41 +0100

    Refactoring

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,46 +1,32 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import { Usuario } from './user.interface';
-import { environment } from '../../environments/environment';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "../../environments/environment";
 
 const API = environment.ApiUrl;
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class UserService {
   constructor(private http: HttpClient) {}
 
-  salvarRegistro(registro: Usuario) {
-    return this.http.post<Usuario>(API + '/user-management/add', registro);
-  }
-
-  addSoftware(usuarioId: string, softwareId: string) {
-    return this.http.post<Usuario>(API + '/user-management/add-software', {
-      usuarioId,
-      softwareId,
-    });
-  }
-
-  removeSoftware(usuarioId: string, softwareId: string) {
-    return this.http.post<Usuario>(API + '/user-management/remove-software', {
-      usuarioId,
-      softwareId,
-    });
-  }
-
   getAll() {
-    return this.http.get<Usuario[]>(API + '/user-management/all');
+    return this.http.get<any[]>(API + "");
   }
 
-  getById(usuarioId: number) {
-    return this.http.get<Usuario>(API + '/user-management/id/' + usuarioId);
+  getDataForGraphPastFourMonth() {
+    return this.http.get<any[]>(API + "/graph-last-4-month");
   }
 
-  removeById(usuarioId: number) {
-    return this.http.delete<Usuario[]>(
-      API + '/user-management/remove/' + usuarioId
-    );
+  getCurrentMonth() {
+    return this.http.get<any>(API + "/current-month");
+  }
+
+  getMonthAnalysis() {
+    return this.http.get<any>(API + "/month-analysis");
+  }
+
+  getMonthAnalysisAgainstLastMonth() {
+    return this.http.get<any>(API + "/month-analysis-comparison");
   }
 }
