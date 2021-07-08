dd9535e278286e5043a166731b0320890d625eb2
Arnaldo C. Bezerra <arnaldo.bezerra@waitrose.co.uk>
2021-07-09 00:17:51 +0100

    Refactoring

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  ApiUrl: 'http://localhost:8080',
-  API_AUTH: 'http://localhost:8080/user-management',
-  SOFTWARE_PUBLIC_KEY: '185a9998-c63e-4fb4-bc7b-27fddd2d0ffb'
+  ApiUrl: "http://localhost:8080",
+  API_AUTH: "http://arcasoftwares.com.br:8080/user-management",
+  SOFTWARE_PUBLIC_KEY: "185a9998-c63e-4fb4-bc7b-27fddd2d0ffb",
 };
